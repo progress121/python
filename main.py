5887fb213d64925f39b692ea1269408247bc29e5
progress121 <83746906+progress121@users.noreply.github.com>
2021-05-06 01:36:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+awnser = input("Enter a Letter from A to D: ")
+
+
+if (awnser == 'A'):
+    print('This Letter is Number 1 Of the alphabet!')
+
+if (awnser == 'a'):
+    print('This Letter is Number 1 Of the alphabet!')
+
+elif(awnser == 'B'):
+    print('This Letter is Number 2 Of the alphabet!')
+
+elif(awnser == 'b'):
+    print('This Letter is Number 2 Of the alphabet!')
+
+elif(awnser == "C"):
+    print('This Letter is Number 3 Of the alphabet!')
+
+elif (awnser == "c"):
+    print('This Letter is Number 3 Of the alphabet!')
+
+elif (awnser == "D"):
+    print('This Letter is Number 4 Of the alphabet!')
+
+elif (awnser == "d"):
+    print('This Letter is Number 4 Of the alphabet!')
+
+elif(awnser == ""):
+    print("Error this Is not a letter")
+
+# I know you can do this alot more simple/easier but I have not learned that yet
